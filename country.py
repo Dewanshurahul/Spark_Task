acb58d7b024aced338f3c832a11224e2a384bdb4
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-10-06 10:50:30 +0530

    Adding: Count Number Of Employee According to Country

---
+++ country.py
@@ -0,0 +1,9 @@
+import os
+os.environ["PYSPARK_PYTHON"] = '/usr/bin/python3'
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+sc = SparkContext()
+spark = SQLContext(sc)
+
+df = spark.read.csv("hdfs://localhost:54310/hadoop/input/HR2m.csv",header=True,inferSchema = True)
+df.groupBy('County').count().show()
