10cb9ce85a3a399da46bb6212d0d4e0f16031c47
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-10-11 15:32:28 +0530

    Refactor:

--- runner.py
+++ runner.py
@@ -1,13 +1,57 @@
-import context, region, employee_joined_by_month, salary_sort, country, city, summary, closing
+import context, task, closing
 
-class Runner(Context, region, employee_joined_by_month, salary_sort, country, city, summary, Closing):
+class Runner(context.Context, task.Task, closing.Closing):
 
-    def run(self):
-        self.context()
-        self.load_data()
-        self.region()
-        self.employee()
-        self.closing()
+    # Calling Context and Data
+    def context_and_data(self):
+        spark = self.context()
+        df = self.load_data(spark)
+        return df
 
-runner = Runner()
-runner.run()
+    # Calling all the task executing based on task-Number Input
+    def run(self, df):
+        while True:
+            var = input('Enter for Processing: ')
+            if var == 'task-1':
+                try:
+                    self.region(df).show()
+                    self.city(df).show()
+                    self.country(df).show()
+                except:
+                    print("Task-1 didn't Executed")
+            elif var == 'task-2':
+                try:
+                    avg, max, min, desc = self.summary(df)
+                    avg.show()
+                    max.show()
+                    min.show()
+                    desc.show()
+                except:
+                    print("Task-2 didn't Executed")
+            elif var == 'task-3':
+                try:
+                    self.orderby_gender_salary(df).show()
+                except:
+                    print("Task-3 didn't Executed")
+            elif var == 'task-4':
+                try:
+                    self.employee_joined_based_on_month(df).show()
+                    self.hikes_granted_based_on_month(df).show()
+                except:
+                    print("Task-4 didn't executed")
+            elif var == 'task-5':
+                try:
+                    self.sort_salary(df).show()
+                except:
+                    print("Task-5 didn't Executed")
+            elif var == 'done':
+                break
+            else:
+                print("Wrong Input!!")
+                continue
+
+
+if __name__ == '__main__':
+    runner = Runner()
+    df = runner.context_and_data()
+    runner.run(df)
