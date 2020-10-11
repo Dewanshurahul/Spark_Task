8be09a434685dd4f763e78d22e27f409d288e195
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-10-11 15:31:08 +0530

    Refactor: Adding remaining task functions and Comments

--- task.py
+++ task.py
@@ -1,17 +1,38 @@
+from pyspark.sql import functions as func
+
 class Task:
 
+    # Count Number of Employees in each Region
     def region(self, df):
-        try:
-            df1 = df.select('Region')
-            df2 = df1.groupBy('Region').count()
-        except:
-            print("Data didn't get injested")
-	return df2
+        return df.groupBy('Region').count()
 
-
+    # Count Number of Employees in each Country
     def country(self, df):
         return df.groupBy('County').count()
 
-
+    # Count Number of Employees in each City
     def city(self, df):
         return df.groupBy('City').count()
+
+    # Generate Employee Summary
+    def summary(self, df):
+        avg = df.agg({'Salary': 'avg'})
+        max = df.agg({'Salary': 'max'})
+        min = df.agg({'Salary': 'min'})
+        return avg, max, min, df.describe()
+
+    # Orderby Gender and Salary
+    def orderby_gender_salary(self, df):
+        return df.sort('Gender','Salary')
+
+    # Hikes Granted based on Month
+    def hikes_granted_based_on_month(self, df):
+        return df.groupBy('Month Name of Joining').agg(func.countDistinct('Last % Hike').alias('Hikes Granted'))
+
+    # Employee joined based on Month
+    def employee_joined_based_on_month(self, df):
+        return df.groupBy('Month Name of Joining').count()
+
+    # Salary Wise sort Employee Data
+    def sort_salary(self, df):
+        return df.sort('Salary')
