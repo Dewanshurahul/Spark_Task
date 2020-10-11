0da79105f6a1d9c8bf247bd7a3bb3839a2838d53
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-10-11 15:47:08 +0530

    Rafactor: Deleting Unnecessary files

--- order_by_gender_salary.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ["PYSPARK_PYTHON"] = '/usr/bin/python3'
-from pyspark import SparkContext
-from pyspark.sql import SQLContext
-sc = SparkContext()
-spark = SQLContext(sc)
-
-df = spark.read.csv("hdfs://localhost:54310/hadoop/input/HR2m.csv",header=True,inferSchema = True)
-df.sort('Gender','Salary').show()
